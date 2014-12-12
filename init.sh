a03e2832f4c7ce34309cb4c10effdb2589482023
BenjyBenton <bbenton4@gmail.com>
2014-12-12 17:14:13 -0500

    Shitdows 1.0

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-url=~/.mozilla/firefox/mwad0hks.default/
-sql_file=places.sqlite
-dest=~/Music/0DL
+set url=C:\Users\Benjy\AppData\Roaming\Mozilla\Firefox\Profiles\gg58zodx.default\
+set sql_file=places.sqlite
+set dest=C:\Users\Benjy\Music\0Piracy
 
-db=$url$sql_file
+set db=%url%%sql_file%
 
-sqlite3 $db < init.ql
-mkdir $dest
+C:\Users\Benjy\sqlite-shell-win32-x86-3080704\sqlite3 %db% < init.ql
+mkdir %dest%
